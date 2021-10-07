388eaf9c858085cec2e5facea063e8cc3264629e
ValerieTurgeon <valerie.turgeon.cem@gmail.com>
2021-10-07 08:49:13 -0400

    FCT Promo Books Migration

---
+++ CrazyBooks/CrazyBooks_DataAccess/Migrations/20211007124720_updateDiff.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrazyBooks_DataAccess.Migrations
+{
+    public partial class updateDiff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Promo",
+                table: "Book",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Promo",
+                table: "Book");
+        }
+    }
+}

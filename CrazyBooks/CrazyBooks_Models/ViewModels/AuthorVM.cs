e1328afc47d904172e950991195d21cbb1ab9f3b
Valérie Turgeon <valerie.turgeon.cem@gmail.com>
2021-10-10 00:15:31 -0400

    FCT Author ViewModel
    Modifier le AuthorVM

--- CrazyBooks/CrazyBooks_Models/ViewModels/AuthorVM.cs
+++ CrazyBooks/CrazyBooks_Models/ViewModels/AuthorVM.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,16 @@ namespace CrazyBooks_Models.ViewModels
 {
   public class AuthorVM
   {
-    public Author Author { get; set; }
-    public Author AuthorDetail { get; set; }
+        public Author Author { get; set; }
+        //public Author AuthorDetail { get; set; }
+        // Author detail
+        
+        public string Biography { get; set; }
+        // Pour le path de l'image
+        [MaxLength(60)]
+        public string Photo { get; set; }
+
+
 
     }
 }
